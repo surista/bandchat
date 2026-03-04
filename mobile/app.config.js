cfd6afe5e26749a73b255c0406da819e4d8605ce
Scott Urista <surista@gmail.com>
2026-03-05 06:45:32 +0900

    v1.04.27 Add bump scripts to mobile/server, sync versions

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'BandChat',
     slug: 'bandchat',
     scheme: 'bandchat',
-    version: '1.04.26',
+    version: '1.04.27',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: false,
       bundleIdentifier: 'com.bandchat.mobile',
-      buildNumber: '10426',
+      buildNumber: '10427',
       infoPlist: {
         NSCameraUsageDescription: 'BandChat needs camera access to take photos for your profile and messages.',
         NSPhotoLibraryUsageDescription: 'BandChat needs photo library access to share images in messages and set your profile picture.',
@@ -30,7 +30,7 @@ export default {
     },
     android: {
       package: 'com.bandchat.mobile',
-      versionCode: 10426,
+      versionCode: 10427,
       adaptiveIcon: {
         backgroundColor: '#1f2937',
         foregroundImage: './assets/android-icon-foreground.png',
