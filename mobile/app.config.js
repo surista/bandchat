7b337f049fe0e7b170dd44b34b72195780bfd69c
Scott Urista <surista@gmail.com>
2026-03-11 00:54:57 +0900

    v1.04.59 Gig time fields, message selection, mobile calendar fix

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -3,7 +3,7 @@ export default {
     name: 'BandChat',
     slug: 'bandchat',
     scheme: 'bandchat',
-    version: '1.04.58',
+    version: '1.04.59',
     orientation: 'portrait',
     icon: './assets/icon.png',
     userInterfaceStyle: 'dark',
@@ -16,7 +16,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: 'com.bandchat.mobile',
-      buildNumber: '10458',
+      buildNumber: '10459',
       infoPlist: {
         NSCameraUsageDescription: 'BandChat needs camera access to take photos for your profile and messages.',
         NSPhotoLibraryUsageDescription: 'BandChat needs photo library access to share images in messages and set your profile picture.',
@@ -36,7 +36,7 @@ export default {
     },
     android: {
       package: 'com.bandchat.mobile',
-      versionCode: 10458,
+      versionCode: 10459,
       adaptiveIcon: {
         backgroundColor: '#1f2937',
         foregroundImage: './assets/android-icon-foreground.png',
